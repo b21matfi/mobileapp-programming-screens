7cee1091be78882ddd6efb794f35fabbcb62bc4c
b21matfi <b21matfi@student.his.se>
2022-05-10 13:52:40 +0200

    Created a second activity, added a button to the second activity and main.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,72 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button Signin;
+    private EditText Username;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Username = findViewById(R.id.myUsername);
+        Signin = findViewById(R.id.myButton);
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "MainActivity button pressed");
+                Log.d("==>", "Username"+Username.getText().toString());
+
+
+
+                Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
+                startActivity(intent);
+
+                intent.putExtra("Username",Username.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+    public MainActivity() {
+        super();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("==>", "MainActivity Resumeed");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>", "MainActivity Started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>", "MainActivity stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>", "MainActivity destroyed");
+    }
+
+
 }
---
+++ app/src/main/java/com/example/screens/ProfileActivity.java
@@ -0,0 +1,65 @@
+package com.example.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+    private Button Signout;
+    private TextView UsernameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Signout = findViewById(R.id.myButton_Signout);
+        UsernameView = findViewById(R.id.myUsernameView);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String username = extras.getString("Username");
+
+        UsernameView.setText(username);
+
+
+        Signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "ProfileActivity button pressed");
+                finish();
+
+            }
+        });
+    }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("==>", "ProfileActivity Resumeed");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>", "ProfileActivity Started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>", "ProfileActivity stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>", "ProfileActivity destroyed");
+    }
+}
